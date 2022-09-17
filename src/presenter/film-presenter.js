a3e49f37294c78c22da3d571b2587be011c0b253
KIlnur <kilnur1988@rambler.ru>
2022-09-17 22:42:50 +0500

    Выделяет в презентере рендеринг компонентов.

--- src/presenter/film-presenter.js
+++ src/presenter/film-presenter.js
@@ -35,6 +35,10 @@ export default class FilmPresenter {
 
   #filter = null;
   #filmComments = [];
+
+  #sortFilterComponent = new SortFiltersView();
+
+
   constructor(mainContainer, filmCardsModel, commentModel) {
     this.#mainContainer = mainContainer;
     this.#filmCardsModel = filmCardsModel;
@@ -46,36 +50,68 @@ export default class FilmPresenter {
     this.#filter = generateFilter(this.#filmCards);
     this.#filmComments = [...this.#commentModel.comments];
 
-    render(new FilmsFiltersView(this.#filter), this.#mainContainer);
-    render(new SortFiltersView(), this.#mainContainer);
+    this.#renderFilmList();
+  };
+
+  #renderFilmList = () => {
+    this.#renderFilters();
+    this.#renderSort();
     render(this.#filmsContainer, this.#mainContainer);
 
     if (this.#filmCards.length === 0) {
-      render(new NoFilmsView(), this.#filmsContainer.element);
+      this.#renderNoFilms();
     } else {
-      render(this.#filmsList, this.#filmsContainer.element);
-      render(this.#filmsListContainer, this.#filmsList.element);
-
-      for (let i = 0; i < Math.min(this.#filmCards.length, FILMS_IN_LIST_COUNT); i++) {
-        this.#renderFilms(this.#filmCards[i], commentFilter(this.#filmCards[i], this.#filmComments));
-      }
-
-      render(this.#topRatedList, this.#filmsContainer.element);
-      render(this.#topRatedListContainer, this.#topRatedList.element);
-      for (let i = 0; i < TOP_RATED_AND_MOST_COMMENTED_FILM_COUNT; i++) {
-        render(new FilmCardView(this.#filmCards[i]), this.#topRatedListContainer.element);
-      }
-
-      render(this.#mostCommentedList, this.#filmsContainer.element);
-      render(this.#mostCommentedListContainer, this.#mostCommentedList.element);
-      for (let i = 0; i < TOP_RATED_AND_MOST_COMMENTED_FILM_COUNT; i++) {
-        render(new FilmCardView(this.#filmCards[i]), this.#mostCommentedListContainer.element);
-      }
+      this.#renderFilmCards();
     }
 
     if (this.#filmCards.length > FILMS_IN_LIST_COUNT) {
-      render(this.#showMoreButtonComponent, this.#filmsList.element);
-      this.#showMoreButtonComponent.setClickHandler(this.#showMoreButtonClickHandler);
+      this.#renderShowMoreButton();
+    }
+
+    this.#renderTopRatedFilms();
+    this.#renderMostCommentedList();
+  };
+
+  #renderFilters = () => {
+    const filterFilmsComponent = new FilmsFiltersView(this.#filter);
+    render(filterFilmsComponent, this.#mainContainer);
+  };
+
+  #renderSort = () => {
+    render(this.#sortFilterComponent, this.#mainContainer);
+  };
+
+  #renderNoFilms = () => {
+    render(new NoFilmsView(), this.#filmsContainer.element);
+  };
+
+  #renderFilmCards = () => {
+    render(this.#filmsList, this.#filmsContainer.element);
+    render(this.#filmsListContainer, this.#filmsList.element);
+
+    for (let i = 0; i < Math.min(this.#filmCards.length, FILMS_IN_LIST_COUNT); i++) {
+      this.#renderFilms(this.#filmCards[i], commentFilter(this.#filmCards[i], this.#filmComments));
+    }
+  };
+
+  #renderShowMoreButton = () => {
+    render(this.#showMoreButtonComponent, this.#filmsList.element);
+    this.#showMoreButtonComponent.setClickHandler(this.#showMoreButtonClickHandler);
+  };
+
+  #renderTopRatedFilms = () => {
+    render(this.#topRatedList, this.#filmsContainer.element);
+    render(this.#topRatedListContainer, this.#topRatedList.element);
+    for (let i = 0; i < TOP_RATED_AND_MOST_COMMENTED_FILM_COUNT; i++) {
+      render(new FilmCardView(this.#filmCards[i]), this.#topRatedListContainer.element);
+    }
+  };
+
+  #renderMostCommentedList = () => {
+    render(this.#mostCommentedList, this.#filmsContainer.element);
+    render(this.#mostCommentedListContainer, this.#mostCommentedList.element);
+    for (let i = 0; i < TOP_RATED_AND_MOST_COMMENTED_FILM_COUNT; i++) {
+      render(new FilmCardView(this.#filmCards[i]), this.#mostCommentedListContainer.element);
     }
   };
 
